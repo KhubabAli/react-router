188782e0fc358167de4e24542c4e1d725ec564dd
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-04-04 14:20:17 +0500

    Set up project

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+
+import Root from "./routes/root"
+import './index.css'
+import ErrorPage from "./error-page.jsx";
+import Contact from "./routes/contact.jsx";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage/>
+    },
+    {
+        path: "contacts/:contactId",
+        element: <Contact />
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
