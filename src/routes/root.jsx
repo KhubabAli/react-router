4a96f33275c4534fe54e07f9b4447937140b60d6
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-04-06 12:06:47 +0500

    Add feature Synchronizing URLs to Form State

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,13 +7,14 @@ import {
     useNavigation
 } from "react-router-dom";
 import {getContacts, createContact} from "../contacts.js";
+import {useEffect} from "react";
 
 export async function loader({request}) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
 
     const contacts = await getContacts(q);
-    return {contacts};
+    return {contacts, q};
 }
 
 export async function action() {
@@ -23,8 +24,14 @@ export async function action() {
 
 
 export default function Root() {
-    const {contacts} = useLoaderData();
+    const {contacts, q} = useLoaderData();
     const navigation = useNavigation();
+
+ 
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
+
 
     return (
         <>
@@ -39,6 +46,7 @@ export default function Root() {
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
                         />
                         <div
                             id="search-spinner"
